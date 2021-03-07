d7e4aa9c99a48a93a7fbadf38723388a4c5bc9a6
zorphis <64968052+zorphis@users.noreply.github.com>
2021-03-07 14:45:50 +0530

    UserUpdates

---
+++ Data/DataMng.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Data
+{
+    public class DataMng
+    {
+        static void main(string[] args)
+        {
+            User user;
+            public static int usercount;
+
+            
+
+        }
+    }
+}
--- Data/User.cs
+++ Data/User.cs
@@ -4,9 +4,19 @@ namespace Data
 {
     public class User
     {
+        DataMng DataMnger;
+        int usercount;
         static void main(string[] args)
         {
-            
+            bool Purchased  = false;
+            bool AlreadyACustomer = false;
+            int CustomerID;
+
+            if(AlreadyACustomer != true)
+            {
+                AlreadyACustomer = true;
+                
+            }
         }
     }
 }
